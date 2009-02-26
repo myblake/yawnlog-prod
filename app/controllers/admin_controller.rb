cca6b3bb56fc033870e18ce15871e2dc77234344
Matthew Blake <mblake@sephiroth-2.local>
2009-02-26 16:35:57 -0500

    more xss

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -35,6 +35,16 @@ class AdminController < ApplicationController
     @sleep_size = @sleeps.length
   end
   
+  def special_method
+    @user_ids=[1,7,8,9,10,16,17,18,26,27,28,29,31,33,37,39,40,41,42,43,44,48,51,52,54,56,59,68]
+    for user in @user_ids
+      @user = Friend.find(user)
+      @user.accepted=true
+      @user.save
+    end
+    flash[:notice] = "all set"
+    redirect_to :controller => "admin", :action => :index
+  end
   
   protected
   def authorize 
