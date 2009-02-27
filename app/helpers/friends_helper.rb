2a8430e29a85d0a7aba748a312e2d0216eb5d29b
Matthew Blake <mblake@sephiroth-2.local>
2009-02-26 20:32:53 -0500

    added pending friend

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,2 +1,7 @@
 module FriendsHelper
+  
+  def pending
+    return !Friend.find(:first, :conditions => ["user_id_2=? and accepted=? and rejected=?",session[:user_id],false,false]).nil?
+  end
+
 end
