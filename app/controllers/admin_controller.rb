b66d3cc9e31b0ec02af16244a488a8434a026d2e
Matthew Blake <mblake@sephiroth-2.local>
2009-03-01 20:22:02 -0500

    added users active today

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,12 @@ class AdminController < ApplicationController
   def index
     @users = User.find(:all)
     @size = @users.length
+    @active_users_today = 0 
+    for user in @users
+      if user.last_login_at && Time.now - user.last_login_at < 86400
+        @active_users_today += 1
+      end
+    end
   end
   
   def edit
